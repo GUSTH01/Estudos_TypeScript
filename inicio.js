4ddc4f30bc5b005643163c09ec02decbbc29815b
gusth01 <gustavofonsecasantos108@gmail.com>
2024-11-25 20:48:48 -0300

    tipos primitivos e Template Strings

--- inicio.js
+++ inicio.js
@@ -1,4 +1,11 @@
-nome = "Gustavo";
-horas = 12;
+// Template Strings
+let nome = 'teste'
+console.log(`Nome: ${nome} <- este`)
 
-console.log("Ola meu nome é:", nome, "são", horas, "Horas da manhã")
+// Tipos primitivos 
+
+let nom = 'gustavo' // string
+let numero = 10 // number que pode ser qualquer tipo de numero
+let nomeAluno // undefined não aponta para local nenhum na memória
+let sobrenome = null // Nulo -> não aponta para local nenhum na memória
+let boolean = true // ou false
