82f28814dafbd2c86a414e87f1b37d9db7a0f854
gusth01 <gustavofonsecasantos108@gmail.com>
2024-11-27 22:32:10 -0300

    Arrays

---
+++ caracteristicasarray.js
@@ -0,0 +1,9 @@
+let nome = 'Pericles'
+let aluno = ['Gustavo', 'Thaua', 'Tais']
+
+// Para inserir um elemento a um array basta colocar o sinal de atribuição
+aluno[2] = 'Péricles'
+
+aluno[aluno.length] = 'Luiza' // atribui no ultimo lugar 
+console.log(aluno.length)// Para saber o tamanho do array
+
