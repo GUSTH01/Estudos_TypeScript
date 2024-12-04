bfcf6cd8eb4ba2ced86bded20600dc4fbe9b95ad
GUSTH01 <gustavofonsecasantos108@gmail.com>
2024-12-03 23:27:19 -0300

    Continuando npm

--- node/app.js
+++ node/app.js
@@ -1,7 +1,2 @@
-const {Pessoa} = require('./modulo1')
-
-const axios = require('axios')
-
-axios('https://www.otaviomiranda.com.br/files/json/pessoas.json')
-.then(response => console.log(response.data))
-.catch(e => console.log(e))
+const path = require('path')// modulo ja existente do node
+console.log(path.resolve(__dirname, '..', '..'))
